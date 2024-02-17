b8837d74dee3d1d9aff6abc4d090ec96b861947d
Magda Dmitruk <dmitrukm@gmail.com>
2024-02-17 22:06:39 +0000

    3rd commit of the game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -32,4 +32,7 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices contain the correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 })
