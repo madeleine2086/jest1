6c043073766415912959e52f31e0083a48374562
Magda Dmitruk <dmitrukm@gmail.com>
2024-02-18 00:17:53 +0000

    4th commit of the game

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -6,7 +6,7 @@
 const { TestScheduler } = require("jest");
 const { hasUncaughtExceptionCaptureCallback } = require("process");
 
-const { game } = require("../game");
+const { game, newGame, showScore } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -35,4 +35,31 @@ describe("game object contains correct keys", () => {
     test("choices contain the correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+})
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+
+    test("should clear the playerMoves array", () => {
+        expect(game.playerMoves.length).toBe(0);
+    });
+
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
+    });
+
+    test("should display zero for the element with an id score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
+    })
+
 })
